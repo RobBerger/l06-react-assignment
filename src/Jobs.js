fda8c51093c8194c7db068245422a49b8adc5d9a
berger <robertberger.dev@gmail.com>
2023-03-01 16:11:36 -0500

    changed json file to js file for data

--- src/Jobs.js
+++ src/Jobs.js
@@ -1,7 +1,29 @@
 import React from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import './data/jobs.json';
 
-function Jobs() {
 
+function Jobs(props) {
+    let jobs = jobs.json;
+    console.log(jobs);
+
+    // function jobs() {
+    //     if (props.jobs === null) return
+    //     return props.jobs.map((jobs) =>
+    //     <ListGroup.Item key={jobs.id}>
+    //         {jobs()}
+    //     </ListGroup.Item>
+    // )}
+
+    return (
+        jobs
+        // <>
+        //     <h1>Jobs</h1>
+        //     <ListGroup className="w-75">
+        //         {jobs()}
+        //     </ListGroup>
+        // </>
+    )
 }
 
 export default Jobs
---
+++ src/data/jobs.js
@@ -0,0 +1,39 @@
+const jobs = [
+{
+    id: 1001,
+    title: "Test Pilot",
+    company: "Lockheed Martin",
+    description: "Can't talk about it... classified.",
+    duration: "Several years"
+},
+{
+    id: 1002,
+    title: "Airline Pilot",
+    company: "Pan-Am",
+    description: "PIC Sikorsky S-42 Clipper",
+    duration: "4 years"
+},
+{
+    id: 1003,
+    title: "Astronaut",
+    company: "Space X",
+    description: "Starship Captain",
+    duration: "2010's to current"
+},
+{
+    id: 1004,
+    title: "Flight Instructor",
+    company: "Air Force",
+    description: "Taught aerobatics to fighter pilots",
+    duration: "5 years"
+},
+{
+    id: 1005,
+    title: "Fighter Pilot",
+    company: "Navy",
+    description: "Top Gun",
+    duration: "Classified"
+}
+]
+
+export default jobs
