7d0de1754caa8a4550bebd1f41a7a4ac08eb9d0b
berger <robertberger.dev@gmail.com>
2023-02-28 20:51:51 -0500

    added export default to Home, Job and About imported all of them to App.js and setup routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Jobs from './Jobs';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="App">
+    <nav>
+
+    </nav>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="about" element={ <About /> } />
+        <Route path="Jobs" element={ <Jobs /> } />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
