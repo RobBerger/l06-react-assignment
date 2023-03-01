74848a8b141b11b5aa4fc9de3b9717f968b5f58c
berger <robertberger.dev@gmail.com>
2023-03-01 14:53:23 -0500

    added a return to About so that everything would show up on the page

--- src/About.js
+++ src/About.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
 
 function About() {
-
-}
+    return (
+    <>
+        <Table>
+            <tbody>
+                <tr>
+                    <td>
+                        <h1>Experience</h1>
+                        <p>Fullstack developer who's taking over the automotive industry.</p>
+                    </td>
+                    <td>
+                        <h1>Dealerships will say "No Way CDK"</h1>
+                    </td>
+                </tr>
+            </tbody>
+        </Table>
+    </>
+)}
 
 export default About
--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,6 @@ import Table from "react-bootstrap/Table";
 function Home() {
     return (
         <>
-
             <Table>
                 <tbody>
                     <tr>
