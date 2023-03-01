cb6cb5635a4af5bc5ee1d9ef483ec3f4fc3c6cd9
berger <robertberger.dev@gmail.com>
2023-03-01 12:40:52 -0500

    moved navbar to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Jobs from './Jobs';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar";
 
 function App() {
   return (
     <div className="App">
-    <nav>
-
-    </nav>
-
     <BrowserRouter>
+    <Navbar bg="light" variant="light">
+                <Container>
+                    <Nav className="me-auto">
+                        <Link to="/" className="nav-link">Home</Link>
+                        <Link to="/jobs" className="nav-link">Jobs</Link>
+                        <Link to="/about" className="nav-link">About</Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
       <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="about" element={ <About /> } />
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
 
 function Home() {
+    return (
+        <>
 
+            <Table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <h1>The Dos Equis Man</h1>
+                            <p>The Most Interesting Man In The World</p>
+                        </td>
+                        <td>
+                            
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
+        </>
+    )
 }
 
 export default Home
